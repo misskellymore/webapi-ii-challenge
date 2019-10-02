db48a42c7d1cd9a425a745f636f23d8d3d96b1bf
Kelly Moreira <moreira.kd@outlook.com>
2019-10-02 09:46:14 -0400

    find method

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const db = require('../data/db.js');
 
 // To create a router, express.Router
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.send('working');
+    // find() returns a promise that resolves to an array of all 
+    // the posts contained in the database.
+    db.find()
+    .then(post => res.status(200).json(post))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err: 'cannot get post info'});
+    });
+    
 });
 
 
